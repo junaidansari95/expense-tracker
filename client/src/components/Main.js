51e5ff68453c0ac64d96c0a8d525c0789f2769a8
Junaid <junaid@Cayzens-MacBook-Air.local>
2020-10-10 00:08:07 +0530

    circular load added

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../App.css';
 import { useSelector } from 'react-redux';
-import { Box, Typography, Divider } from '@material-ui/core';
+import { Box, Typography, CircularProgress, Divider } from '@material-ui/core';
 import InputForm from './InputForm';
 import ExpenseList from './ExpenseList';
 
 export default () => {
-    const { expenses } = useSelector(state => state.expenses);
+    const { expenses, isGetExpenseRequestLoading, isPostRequestLoading, isUpdateRequestLoading, isDeleteRequestLoading } = useSelector(state => state.expenses);
     let total = 0;
     if(undefined !== expenses && expenses.length){
         expenses.forEach(exp => total += exp.amount);
     }
+    if(isGetExpenseRequestLoading || isPostRequestLoading || isUpdateRequestLoading || isDeleteRequestLoading)
+        return(<Box style={{ display: 'flex', justifyContent: 'center', paddingTop: '25%' }}>
+                    <CircularProgress size={70} />
+                </Box>)
+    else
     return (
         <>
             <Box className="header">
